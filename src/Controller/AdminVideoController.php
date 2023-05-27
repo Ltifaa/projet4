ebf1a481f332d06de1b7d84a4e758cd2c2fb89f9
Latifa Chetouani <125277378+Ltifaa@users.noreply.github.com>
2023-05-27 15:55:46 +0200

    nav-bar latteral

--- src/Controller/AdminVideoController.php
+++ src/Controller/AdminVideoController.php
@@ -10,18 +10,18 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/admin/video/conroller')]
+#[Route('/admin/video')]
 class AdminVideoConrollerController extends AbstractController
 {
-    #[Route('/', name: 'app_admin_video_conroller_index', methods: ['GET'])]
+    #[Route('/', name: 'app_admin_video_index', methods: ['GET'])]
     public function index(VideoRepository $videoRepository): Response
     {
-        return $this->render('admin_video_conroller/index.html.twig', [
+        return $this->render('admin_video/index.html.twig', [
             'videos' => $videoRepository->findAll(),
         ]);
     }
 
-    #[Route('/new', name: 'app_admin_video_conroller_new', methods: ['GET', 'POST'])]
+    #[Route('/new', name: 'app_admin_video_new', methods: ['GET', 'POST'])]
     public function new(Request $request, VideoRepository $videoRepository): Response
     {
         $video = new Video();
@@ -31,24 +31,24 @@ class AdminVideoConrollerController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $videoRepository->save($video, true);
 
-            return $this->redirectToRoute('app_admin_video_conroller_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_admin_video_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->renderForm('admin_video_conroller/new.html.twig', [
+        return $this->renderForm('admin_video/new.html.twig', [
             'video' => $video,
             'form' => $form,
         ]);
     }
 
-    #[Route('/{id}', name: 'app_admin_video_conroller_show', methods: ['GET'])]
+    #[Route('/{id}', name: 'app_admin_video_show', methods: ['GET'])]
     public function show(Video $video): Response
     {
-        return $this->render('admin_video_conroller/show.html.twig', [
+        return $this->render('admin_video/show.html.twig', [
             'video' => $video,
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_admin_video_conroller_edit', methods: ['GET', 'POST'])]
+    #[Route('/{id}/edit', name: 'app_admin_video_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Video $video, VideoRepository $videoRepository): Response
     {
         $form = $this->createForm(VideoType::class, $video);
@@ -57,22 +57,22 @@ class AdminVideoConrollerController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $videoRepository->save($video, true);
 
-            return $this->redirectToRoute('app_admin_video_conroller_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_admin_video_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->renderForm('admin_video_conroller/edit.html.twig', [
+        return $this->renderForm('admin_video/edit.html.twig', [
             'video' => $video,
             'form' => $form,
         ]);
     }
 
-    #[Route('/{id}', name: 'app_admin_video_conroller_delete', methods: ['POST'])]
+    #[Route('/{id}', name: 'app_admin_video_delete', methods: ['POST'])]
     public function delete(Request $request, Video $video, VideoRepository $videoRepository): Response
     {
         if ($this->isCsrfTokenValid('delete'.$video->getId(), $request->request->get('_token'))) {
             $videoRepository->remove($video, true);
         }
 
-        return $this->redirectToRoute('app_admin_video_conroller_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('app_admin_video_index', [], Response::HTTP_SEE_OTHER);
     }
 }
