d08c4147d29bae32af90b093dc62e947ec762043
Latifa Chetouani <125277378+Ltifaa@users.noreply.github.com>
2023-05-27 12:26:08 +0200

    controller et crud & user

---
+++ src/Form/VideoType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Video;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class VideoType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('updatedAt')
+            ->add('time')
+            ->add('sponsor')
+            ->add('slug')
+            ->add('categorie')
+            ->add('relation')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Video::class,
+        ]);
+    }
+}
